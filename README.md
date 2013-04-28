1745144891bbeecd8bba28b923d3c0ece9f50acb
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2013-04-28 14:05:01 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Revy Riders Map
+
+* **Version:** 1.0
+* ~Current Version:1.0~
+* **Requires at least:** 3.5
+* **Tested up to:** 3.6
+* **License:** GPLv2
+
+Description
+-----------
+
+A Google Map showcasing Revy Riders Dirt Bike Club trails and track in Revelstoke, BC.
+
+Usage
+-----
+
+Use shortcode ``` [revyriders-map] ``` in a page with no sidebar for best results. That's it!
---
+++ revyriders-map.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package Revy Riders Map
+ * @since   April 28, 2013
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+/*
+Plugin Name: Revy Riders Map
+Plugin URI: https://derekmarcinyshyn.github.com/revy riders map
+Description: A Google Map showcasing Revy Riders Dirt Bike Club trails and track in Revelstoke, BC.
+Author: Derek Marcinyshyn
+Author URI: http://derek.marcinyshyn.com
+Version: 1.0
+License: GPLv2
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+// Exit if called directly
+defined( 'ABSPATH' ) or die( "Cannot access pages directly." );
+
+// Plugin version
+define( 'REVYRIDERSMAP_VERSION', '1.0');
